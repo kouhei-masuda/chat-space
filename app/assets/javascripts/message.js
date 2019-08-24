f3042f701dc62f71317028db716671104b8cc5fa
kouhei-masuda <kohei.masuda@di-v.co.jp>
2019-08-24 17:06:00 +0900

    fix

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,8 @@
 $(function(){
   function buildHTML(message){
-    if(message.content!=null  && message.image==null){
-
-    
+    var  tag_image = '';
+    message.image ==　null ? tag_image = '' :tag_image =  `<img class="lower-message__image" src="${message.image}">`
+   
     var html = `
     <div class='message'>
      <div class='message__upper-info'>
@@ -15,47 +15,16 @@ $(function(){
      </div>
      <p class="lower-message__content">
         ${message.content}
-     </p>
-    </div>`
+     </p>` 
+        + tag_image +
+    `</div>`
     return html;
-    }else if(message.content == null  && message.image != null){
-      var html = `
-      <div class='message'>
-     <div class='message__upper-info'>
-      <div class='message__upper-info__talker'>
-        ${message.user_name}
-      </div>
-      <div class='message__upper-info__date'>
-        ${message.time}
-      </div>
-     </div>
-     <img class="lower-message__image" src="${message.image}">
-    </div>`
-    return html;
-    }else{
-      var html = `
-      <div class='message'>
-     <div class='message__upper-info'>
-      <div class='message__upper-info__talker'>
-        ${message.user_name}
-      </div>
-      <div class='message__upper-info__date'>
-        ${message.time}
-      </div>
-     </div>
-     <p class="lower-message__content">
-     ${message.content}
-     </p>
-     <img class="lower-message__image" src="${message.image}">
-    </div>`
-    return html;
-    }
+    
   }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var href = window.location.href;
-    console.log(this)
     $.ajax({
       url: href,
       type: "POST",
@@ -66,9 +35,8 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.messages').append(html);
-      $('.input-box__text').val('');
-      $('.input-box__image__file').val('');  
+      $('.messages').append(html);  
+      $('.input-box__text').val('')
       $('.messages').animate({scrollTop:$(".messages")[0].scrollHeight}, 'fast');
     })
     .fail(function(){
