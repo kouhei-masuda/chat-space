5efe7d2e46c6e78622e40ec9417afa78ba285463
kouhei-masuda <kohei.masuda@di-v.co.jp>
2019-08-27 11:11:02 +0900

    fix

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,7 +1,7 @@
 $(function() {
 
   var search_list = $("#user-search-result");
-  var member_list = $("#member_search_result");
+  var member_list = $("#chat-group-users");
 
   function appendUsers(user) {
     var html =`<div class="chat-group-user clearfix">
@@ -16,14 +16,14 @@ $(function() {
   function appendMembers(name, user_id) {
     var html =`<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
                 <input name='group[user_ids][]' type='hidden' value=${ user_id }>
-                <p class='c hat-group-user__name'>${ name }</p>
-                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
-              </div>`
+                <p class='chat-group-user__name'>${ name }</p>
+                <p class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</p>
+                </div>`
 
 　　 member_list.append(html);
   }
 
-  function appendNoUsers(info) {
+  function noUsers(info) {
     var html =`<div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">${ info }</p>
               </div>`
@@ -50,7 +50,7 @@ $(function() {
             });
           }
           else {
-            appendNoUsers("一致するユーザーはいません");
+            noUsers("一致するユーザーはいません");
           }
         })
       .fail(function() {
@@ -60,6 +60,7 @@ $(function() {
 
     $(function(){
       $(document).on('click', '.user-search-add', function() {
+        console.log('aa');
         var name = $(this).data("user-name");
         var user_id = $(this).data("user-id");
         $(this).parent().remove();
