fbac15dab9205fbc6a87216d4cefc3dea3a8ee0c
kouhei-masuda <kohei.masuda@di-v.co.jp>
2019-08-30 22:14:32 +0900

    fix_deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,22 +40,22 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock '<Capistranoのバージョン>'
+lock '3.11.1'
 
 # Capistranoのログの表示に利用する
-set :application, '自身のアプリケーション名'
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:<Githubのユーザー名>/<レポジトリ名>.git'
+set :repo_url,  'git@github.com:kouhei-masuda/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '<このアプリで使用しているrubyのバージョン>' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
 # どの公開鍵を利用してデプロイするか
-set :ssh_options, auth_methods: ['publickey'], keys: ['<ローカルPCのEC2インスタンスのSSH鍵(pem)へのパス>']
+set :ssh_options, auth_methods: ['publickey'], keys: ['f7b2f3c5f92c2bc970bd3b0fb300bfee34966631e40086d596236a48857fbc2f20ab154a540f6e4ce483858f407f863fb72c9d79cf52f79b43d5c73494ef200c']
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
